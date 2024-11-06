88c347d9b539f94eca60e54f0a150c3f92149465
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-11-06 15:28:14 -0300

    Definir un array y acceder de manera arbitraria a diferentes elementos

---
+++ products.js
@@ -0,0 +1,44 @@
+// Micro desafío 1: 
+
+// El Tech Leader nos informa que debemos efectuar un código 
+// que permita calcular el saldo de la cuenta 
+// de un cliente de la Institución Financiera. 
+
+// Define un array que contenga un conjunto de seis (6) productos de electrodomésticos.
+
+let electros = ["cocina", "microondas", "airfryer", "heladera", "freezer", "lavarropas"];
+
+
+// Ejecutar sobre el array creado las siguientes acciones:
+
+// a. Acceder de manera arbitraria a diferentes elementos del array. 
+
+// Recuerda mostrar por la consola los resultados.
+
+let elPrimerElemento = electros[0];
+
+let elSegundoElemento = electros[1];
+
+let elTercerElemento = electros[2];
+
+console.log(elPrimerElemento, elSegundoElemento, elTercerElemento)
+
+
+// b. Extraer el primer elemento del array y agregarlo al final del mismo.
+
+// c. Agregar al final del array dos (2) nuevos productos.
+
+// d. Mostrar por la consola la cantidad de elementos que contiene el array.
+
+// e. Buscar un elemento del array y crear una condición responsable de establecer 
+// si existe o no el producto dentro del array. De existir, se le debe mostrar el mensaje: 
+// “Producto encontrado”. En el caso contrario mostrar el mensaje “El producto buscado no existe”.
+
+// f. Unificar todos los elementos del array en una cadena de texto (string), separando los elementos por espacios en blanco.
+
+// g. Determinar la cantidad de elementos que posee la cadena de texto obtenida.
+
+// h. Cambiar el nombre de algún producto de la cadena de texto por otro, valiéndose de la función adecuada para ello.
+
+// i.Con la cadena de texto obtenida generar un nuevo array con cada una de las palabras de la cadena de texto. 
+// Tener en cuenta que los elementos deben estar separados por una coma. En todos los casos se debe mostrar por consola el resultado obtenido.
